2830ad2475522801c31be10f1fe8dd893698590c
degtev <mr.dolte@gmail.com>
2024-04-19 21:11:50 +0300

    Update files

--- .description.php
+++ .description.php
@@ -1,12 +1,12 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
+use Bitrix\Main\Localization\Loc;
 $arComponentDescription = array(
-    "NAME" => "Конструктор форм",
+    "NAME" => Loc::getMessage("DESC_COMPONENT_NAME"),
     "DESCRIPTION" => "",
     "ICON" => "",
     "PATH" => array(
-        "ID" => "custom",
+        "ID" => Loc::getMessage("DESC_COMPONENTS_ID"),
     ),
 );
 ?>
---
+++ lang/ru/.description.php
@@ -0,0 +1,3 @@
+<?php
+$MESS["DESC_COMPONENT_NAME"] = "Конструктор форм";
+$MESS["DESC_COMPONENTS_ID"] = "degtev: компоненты";
